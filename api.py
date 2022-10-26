7f0bee5ceca164dcc1649ded52bc8c12d2399147
alb3rtov <albertovmrm@gmail.com>
2022-10-26 21:51:48 +0200

    Store usernames and password in shadow file

--- api.py
+++ api.py
@@ -1,58 +1,95 @@
 #!/usr/bin/env python3
 
-import uuid, os
+import uuid, os, hashlib
 from flask import jsonify, Flask, request
-from flask_restful import reqparse, Resource, Api
-
-__version__ = 'v0.0.1-alpha'
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-users = []
-passwords = []
-tokens = []
+''' Global variables '''
+__version__ = 'v0.0.1-alpha'
+tokens_dict = {}
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+''' Global functions '''
+def generate_access_token():
+    ''' Generate random token for a new user '''
+    return uuid.UUID(bytes=os.urandom(16), version=4)
 
+def encrypt_password(password):
+    ''' Encrypt password using SHA256 algorithm'''
+    return hashlib.sha256(password.encode()).hexdigest()
+
+''' Classes '''
 class Version(Resource):
+    ''' Version class '''    
     def get(self):
+        ''' Return software version '''
         return {'version' : __version__}
 
 class Login(Resource):
+    ''' Login class '''
     def check_credentials(self, username, password):
-        try:
-            user_id = users.index(username)
-            pass_id = passwords.index(password)
-            if (user_id == pass_id):
+        ''' Check if credentials are correct '''
+        shadow_file = open('shadow', 'r')
+        lines = shadow_file.readlines()
+        shadow_file.close()
+        
+        for line in lines:
+            credentials = line.split(":")
+            if (credentials[0] == username and credentials[1].strip() == encrypt_password(password)):
                 return True
-            else:
-                return False
-        except ValueError:
-            return False     
+        
+        return False
 
     def post(self):
+        ''' Process POST request '''
         json_data = request.get_json(force=True)
         un = json_data['username']
         pw = json_data['password']
+
         if (self.check_credentials(un,pw)):
-            return jsonify(access_token=tokens[users.index(un)])
+            if un in tokens_dict:
+                return jsonify(access_token=tokens_dict[un])
+            else:
+                token = generate_access_token()
+                tokens_dict[un] = token
+                return jsonify(access_token=token)
         else:
             return "Error, user or password incorrect.", 401
-  
+
 class SignUp(Resource):
+    ''' SignUp class '''
+    def create_directory(self, username):
+        ''' Create directory of user if does not exists '''
+        if not os.path.isdir(username):
+            os.system("mkdir " + username)
+
+    def register_user(self, username, password):
+        ''' Register new user in shadow file '''
+        shadow_file = open('shadow', 'a')
+        credentials = ""
+
+        if (os.stat("shadow").st_size != 0):
+            credentials = "\n"
+
+        credentials += username + ":" + encrypt_password(password)
+        shadow_file.writelines(credentials)
+        shadow_file.close()
+
     def check_username(self, username):
-        if (username in users):
+        ''' Check if username already exists '''
+        shadow_file = open('shadow', 'r')
+        lines = shadow_file.readlines()
+        shadow_file.close()
+
+        if (username in lines):
             return True
         else:
             return False
 
-    def generate_access_token(self):
-        return uuid.UUID(bytes=os.urandom(16), version=4)
-
     def post(self):
+        ''' Process POST request '''
         json_data = request.get_json(force=True)
         un = json_data['username']
         pw = json_data['password']
@@ -60,13 +97,13 @@ class SignUp(Resource):
         if (self.check_username(un)):
             return "Error, username " + un + " already exists.", 401
         else:
-            users.append(un)
-            passwords.append(pw)
-            token = self.generate_access_token();
-            tokens.append(token)
+            self.register_user(un, pw)
+            self.create_directory(un)
+
+            token = generate_access_token();
+            tokens_dict[un] = token
             return jsonify(access_token=token)
 
-api.add_resource(HelloWorld, '/')
 api.add_resource(Version, '/version')
 api.add_resource(Login, '/login')
 api.add_resource(SignUp, '/signup')
